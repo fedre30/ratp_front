cdc0b114e7d960063ef1c149db9e12f7efec2e04
Draktors <enzo.hespel@hotmail.fr>
2019-04-16 21:57:33 +0200

    remove useless file

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
 import HomeContainer from "../../views/home/HomeContainer";
 
 class App extends Component {
