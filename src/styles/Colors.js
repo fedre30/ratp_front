ca0f5f4343d0146e595ec71e9af2a39e5cc1c8ac
Draktors <enzo.hespel@hotmail.fr>
2019-04-16 21:29:36 +0200

    add prettier

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -1,12 +1,12 @@
 const Colors = {
-    background: "#10154C",
-    primary: "#3A3D60",
-    secondary: "#A4CBD8",
-    tertiary: "#00AA91",
-    text: "#FFF",
-    red: "#D75A4A",
-    yellow: "#FFBE00",
-    lightgrey: "#F0F0F0"
+  background: "#10154C",
+  primary: "#3A3D60",
+  secondary: "#A4CBD8",
+  tertiary: "#00AA91",
+  text: "#FFF",
+  red: "#D75A4A",
+  yellow: "#FFBE00",
+  lightgrey: "#F0F0F0",
 };
 
 export default Colors;
--- src/views/home/HomeContainer.js
+++ src/views/home/HomeContainer.js
@@ -2,18 +2,11 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import Home from "./Home";
 
-
 class HomeContainer extends React.Component {
-    state = {
-
-    }
-    componentDidMount(){
-
-
-
-    }
-    render() {
-        return <Home />;
-    }
+  state = {};
+  componentDidMount() {}
+  render() {
+    return <Home />;
+  }
 }
 export default withRouter(HomeContainer);
