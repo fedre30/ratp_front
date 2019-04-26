17b0dac1cb8fbb3b00bfa11b6277e433174143b0
fedre30 <alfanofederica95@gmail.com>
2019-04-26 11:46:33 +0200

    Stations points and Pollutions indexes

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,27 +1,40 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import styled from "styled-components";
+import { colors } from "styles/const";
 
-import { ComposableMap, ZoomableGroup, Geographies, Geography } from "react-simple-maps";
-import geography from "scripts/citeair.json";
-// import { Motion, spring } from "react-motion";
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+  Markers,
+  Marker,
+} from "react-simple-maps";
+import geography from "scripts/citeair_april.json";
+import { Motion, spring } from "react-motion";
+import stations from "scripts/positions-geographiques-des-stations-du-reseau-ratp";
+import pollution from "scripts/citeair_pollution";
 
 class MapComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      markers: [],
-      center: [0, 0],
+      stations: stations,
+      pollution: pollution,
+      center: [2.35, 48.85],
       zoom: 1,
       modal: "",
       show: false,
       currentID: undefined,
+      currentPollutionIndex: "pm10",
     };
 
     this.handleZoomIn = this.handleZoomIn.bind(this);
     this.handleZoomOut = this.handleZoomOut.bind(this);
     this.handleCityClick = this.handleCityClick.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.changePollutionIndex = this.changePollutionIndex.bind(this);
   }
 
   // RENDER
@@ -29,48 +42,140 @@ class MapComponent extends Component {
   render() {
     return (
       <MapWrapper>
-        <ComposableMap
-          projectionConfig={{
-            scale: 100000,
+        <button onClick={this.handleZoomIn}>Zoom in</button>
+        <button onClick={this.handleZoomOut}>zoom out</button>
+        <button onClick={this.handleReset}>reset</button>
+
+        <button
+          onClick={() => {
+            this.changePollutionIndex("pm10");
           }}
-          width={980}
-          height={551}
-          style={{
-            width: "100%",
-            height: "auto",
-          }}
-          projection="mercator"
         >
-          <ZoomableGroup center={[2.3, 48.85]} zoom={this.state.zoom}>
-            <Geographies geography={geography}>
-              {(geographies, projection) =>
-                geographies.map((geography, i) => (
-                  <Geography
-                    key={i}
-                    geography={geography}
-                    projection={projection}
-                    style={{
-                      default: {
-                        fill: "#000",
-                        stroke: "#fff",
-                        strokeWidth: 1,
-                        outline: "none",
-                      },
-                      hover: {
-                        fill: "red",
-                        stroke: "#000",
-                        strokeWidth: 0.3,
-                      },
-                      pressed: {
-                        fill: "#CEA6E9",
-                      },
-                    }}
-                  />
-                ))
-              }
-            </Geographies>
-          </ZoomableGroup>
-        </ComposableMap>
+          Pm10
+        </button>
+        <button
+          onClick={() => {
+            this.changePollutionIndex("no2");
+          }}
+        >
+          No2
+        </button>
+        <button
+          onClick={() => {
+            this.changePollutionIndex("o3");
+          }}
+        >
+          O3
+        </button>
+
+        <Motion
+          defaultStyle={{
+            zoom: 1,
+            x: 0,
+            y: 20,
+          }}
+          style={{
+            zoom: spring(this.state.zoom, { stiffness: 310, damping: 30 }),
+            x: spring(this.state.center[0], { stiffness: 310, damping: 30 }),
+            y: spring(this.state.center[1], { stiffness: 310, damping: 30 }),
+          }}
+        >
+          {({ zoom, x, y }) => (
+            <ComposableMap
+              projectionConfig={{
+                scale: 250000,
+              }}
+              width={1240}
+              height={700}
+              projection="mercator"
+            >
+              <ZoomableGroup center={[x, y]} zoom={zoom}>
+                <Geographies geography={geography}>
+                  {(geographies, projection) =>
+                    geographies.map((geography, i) => (
+                      <Geography
+                        key={i}
+                        geography={geography}
+                        projection={projection}
+                        style={{
+                          default: {
+                            fill: colors.primary,
+                            stroke: colors.secondary,
+                            strokeWidth: 0.7,
+                            outline: "none",
+                          },
+                          hover: {
+                            fill: colors.primary,
+                            stroke: colors.secondary,
+                            strokeWidth: 0.7,
+                            outline: "none",
+                          },
+                          pressed: {
+                            fill: colors.secondary,
+                            outline: "none",
+                          },
+                        }}
+                      />
+                    ))
+                  }
+                </Geographies>
+                <Markers>
+                  {this.state.pollution.map((marker, i) => (
+                    <Marker
+                      key={i}
+                      marker={marker.geometry}
+                      style={{
+                        default: { fill: colors.text, cursor: "pointer", opacity: 0.1 },
+                        hover: { fill: colors.text, cursor: "pointer", opacity: 0.1 },
+                        pressed: {
+                          fill: "#FFFFFF",
+                          cursor: "pointer",
+                          outline: "none",
+                          opacity: 0.1,
+                        },
+                      }}
+                    >
+                      <circle
+                        cx={0}
+                        cy={0}
+                        r={marker.fields[this.state.currentPollutionIndex]}
+                        style={{
+                          stroke: colors.text,
+                          strokeWidth: 3,
+                          opacity: 0.3,
+                        }}
+                      />
+                    </Marker>
+                  ))}
+                </Markers>
+                <Markers>
+                  {this.state.stations.map((marker, i) => (
+                    <Marker
+                      key={i}
+                      marker={marker.geometry}
+                      style={{
+                        default: { fill: colors.tertiary, cursor: "pointer" },
+                        hover: { fill: colors.primary, cursor: "pointer" },
+                        pressed: { fill: "#FFFFFF", cursor: "pointer", outline: "none" },
+                      }}
+                    >
+                      <circle
+                        cx={0}
+                        cy={0}
+                        r={3}
+                        style={{
+                          stroke: colors.tertiary,
+                          strokeWidth: 3,
+                          opacity: 0.9,
+                        }}
+                      />
+                    </Marker>
+                  ))}
+                </Markers>
+              </ZoomableGroup>
+            </ComposableMap>
+          )}
+        </Motion>
       </MapWrapper>
     );
   }
@@ -98,8 +203,14 @@ class MapComponent extends Component {
 
   handleReset() {
     this.setState({
-      center: [0, 20],
+      center: [2.35, 48.85],
       zoom: 1,
+    });
+  }
+
+  changePollutionIndex(index) {
+    this.setState({
+      currentPollutionIndex: index,
     });
   }
 
