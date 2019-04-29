c052350afd4ac28c3d39399ae496b55353d546af
Draktors <enzo.hespel@hotmail.fr>
2019-04-29 11:57:49 +0200

    add ui on station search bar

--- src/views/home/station-search.js
+++ src/views/home/station-search.js
@@ -3,10 +3,14 @@ import styled from "styled-components";
 import { rem } from "polished";
 import { colors } from "styles/const";
 
+import search from "images/icons/search.svg";
+import plus from "images/icons/plus.svg";
+
 const StationChoice = styled.div`
   background: #fff;
   width: ${rem(313)};
   height: ${rem(103)};
+  padding: ${rem(21)} ${rem(35)};
   border-right: 1px solid grey;
 `;
 
@@ -14,31 +18,89 @@ const StationPlus = styled.div`
   background: #fff;
   width: ${rem(103)};
   height: ${rem(103)};
+  padding: ${rem(39)} 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
 `;
 
 const SearchBtn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background: ${colors.tertiary};
   width: ${rem(241)};
   height: ${rem(103)};
   border-top-right-radius: 5px;
   border-bottom-right-radius: 5px;
   padding: ${rem(39)} 0;
+  cursor: pointer;
   & > p {
-    text-align: center;
+    color: #fff;
   }
 `;
 
-const StationSearch = () => (
-  <div style={{ display: "flex" }}>
-    <StationChoice>
-      <p>Station</p>
-    </StationChoice>
-    <StationChoice>
-      <p>Station</p>
-    </StationChoice>
-    <StationPlus />
-    <SearchBtn>Rechercher</SearchBtn>
-  </div>
-);
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  border: none;
+  font-size: ${rem(20)};
+  font-family: "roboto";
+
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    color: #3a3d60;
+    opacity: 0.6;
+  }
+`;
+
+class StationSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchBtn: 2,
+    };
+  }
+  render() {
+    return (
+      <div style={{ display: "flex" }}>
+        <StationChoice>
+          <p style={{ marginBottom: rem(15) }}>STATION</p>
+          <InputWrapper>
+            <img
+              src={search}
+              alt=""
+              style={{ marginRight: rem(4), width: rem(16), height: rem(16) }}
+            />
+            <Input placeholder="Choisissez une station" />
+          </InputWrapper>
+        </StationChoice>
+        <StationChoice>
+          <p style={{ marginBottom: rem(15) }}>STATION</p>
+          <InputWrapper>
+            <img
+              src={search}
+              alt=""
+              style={{ marginRight: rem(4), width: rem(16), height: rem(16) }}
+            />
+            <Input placeholder="Choisissez une station" />
+          </InputWrapper>
+        </StationChoice>
+        <StationPlus>
+          <img src={plus} alt="" />
+        </StationPlus>
+        <SearchBtn>
+          <p>Rechercher</p>
+        </SearchBtn>
+      </div>
+    );
+  }
+}
 
 export default StationSearch;
